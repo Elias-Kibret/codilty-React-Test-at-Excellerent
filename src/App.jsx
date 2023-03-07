9fe5f142b295c8ea88cae29d88fd507ebbe2e423
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-03-07 21:34:24 +0300

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 const Rating = ({ name, rate, content }) => {
-  const array = [...Array(rate).keys()]
-  
   return (
     <div>
       <h3>Rating</h3>
       {
-        [...Array(rate).keys()].map((item,index) => {
-          return <span key={index}>&#9733;
-            
+        [...Array(5).keys()].map((item,index) => {
+          return <span key={index}>
+            {index<rate?<>&#9733;</>:<>&#9734;</>}
           </span>
-        })
-        
-        
-      }
-      {
-        [...Array(5-rate).keys()].map((item,index) => {
-          return <span key={index}>&#9734;</span>
-        })
+        })    
       }
       <h3>{name}</h3>
       <p>{content }</p>
@@ -33,18 +24,11 @@ const AverageRating = ({ rating }) => {
     <div>
       <h3>Average Rating</h3>
       {
-        [...Array(aveRate).keys()].map((item,index) => {
-          return <span key={index}>&#9733;
-            
+        [...Array(5).keys()].map((item,index) => {
+          return <span key={index} >
+            {index<aveRate?<>&#9733;</>:<>&#9734;</>}  
           </span>
-        })
-        
-        
-      }
-      {
-        [...Array(5-aveRate).keys()].map((item,index) => {
-          return <span key={index}>&#9734;</span>
-        })
+        })  
       }
     </div>
   )
@@ -53,36 +37,35 @@ const AverageRating = ({ rating }) => {
 function App() {
   const rating = [
     {
+      id:0,
       name: 'Elias',
       content: 'I like this product',
-      rate:5
-    },
-    {
-      name: 'Kibret',
-      content: "it's good ,I Like it some how",
       rate:4
     },
-    {
-      name: 'Lidia',
-      content: 'I hate this product',
-      rate:2
+   { id:1,
+       name: 'Kibret',
+       content: "it's good ,I Like it some how",
+      rate:3
+      },
+     { id:2,
+       name: 'Lidia',
+       content: 'I hate this product',
+       rate:0
      },
    ]
   return (
     <div className="App">
       <AverageRating rating={rating} />
-      
       {
         rating.map((item,index) => {
           return (
-            <Rating key={index} {...item} />
+            <Rating key={item.id} {...item} />
           )
         })
     }
-
-
     </div>
   )
 }
 export {Rating,AverageRating}
 export default App
+                                   
